e358a6c27ce073aec2f27f9ea6b0f1867aa9cb96
serg060606 <serg060606@gmail.com>
2016-01-19 23:00:12 +0200

    Fixed backup register function. Its working now.

--- my_tools/log_parcer/remove_IT.py
+++ my_tools/log_parcer/remove_IT.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.7 -tt
 
-import re, os, sys, commands, shutil, subprocess
+import re, os, sys, commands, shutil
 
 
 def List(filename):
@@ -8,40 +8,33 @@ def List(filename):
 	text = f.read()
   
   # Get list of paths to file and attention reg values
-	FilePath_match = re.search(r'\w:\\', text)
-	RegPath_match = re.search(r'HKEY', text)
-	if not FilePath_match:
+	check_match = re.search(r'\w:\\|HKEY', text)
+	if not check_match:
 		sys.stderr.write('Attention lines not found !\n')
 		sys.exit(1)
-#  if not RegPath_match:
-#	sys.stderr.write('Attention lines not found !\n')
-#   sys.exit(1)
-#  else:
-#    pass
-  
-# 20:02:59.492	Found	adriver.ru	Tracking Cookie	C:\Users\Knight\AppData\Roaming\Mozilla\Firefox\Profiles\f2qeychw.default\cookies.sqlite: adriver.ru
-# 20:04:01.365	Found	Crossrider	Adware, Toolbar	HKEY_CURRENT_USER\Software\AppDataLow\Software\Crossrider
-  
-	targetlist = re.findall('[A-Z]:.+|HKEY.+', text)
+
+	target_list = re.findall('[A-Z]:.+|HKEY.+', text)
 	
-	cleanlist = []
-	for idx in range(len(targetlist)):
-		if 'Mozilla\Firefox\Profiles' in targetlist[idx]:
+	clean_list = []
+	for idx in range(len(target_list)):
+		if 'Mozilla\Firefox\Profiles' in target_list[idx]:
 			match = re.search(r'[A-Z]:\\\w+\\\w+\\\w+\\\w+\\Mozilla\\Firefox\\Profiles\\\w+\.\w+', text)
-			if match.group() not in cleanlist:
-				cleanlist.append(match.group())
+			if match.group() not in clean_list:
+				clean_list.append(match.group())
 		else:
-			cleanlist.append(targetlist[idx])
+			clean_list.append(target_list[idx])
 		
-	return cleanlist
+	return clean_list
 	
 	
-def backup_and_remove(list):
+def backup(list):
 	backup_dir = 'remove_it_backup'
 	if os.path.exists(backup_dir):
-		pass
+		backup_dir = os.path.abspath(backup_dir)
 	else:
 		os.mkdir(backup_dir)
+		backup_dir = os.path.abspath(backup_dir)
+		
 	for text in list:
 		if re.search(r'\w:\\', text):
 			if os.path.exists(text):
@@ -50,8 +43,9 @@ def backup_and_remove(list):
 						
 						clean_path = '\\'.join((text.split('\\'))[1:-1])
 						if os.path.exists(backup_dir + '\\' + clean_path):
-							print 'Making backup of', text, '\n', 'in', os.path.abspath(backup_dir), '\n'
+							print 'Making backup of', text, '\n', 'in', backup_dir, '\n'
 							shutil.copy(text, backup_dir + '\\' + clean_path)
+							
 						else:
 							os.makedirs(backup_dir + '\\' + clean_path)
 							shutil.copy(text, backup_dir + '\\' + clean_path)
@@ -66,38 +60,21 @@ def backup_and_remove(list):
 				print 'Path: ',text,"doesn't exist"
 
 		elif re.search('HKEY.+', text):
-			
-			#reg_query = 'REG QUERY ' + '"' + text + '"'
-			#text = '"'+text+'"'
-			print text
-			reg_query = subprocess.call(['REG', 'QUERY', '"'+ text +'"'])
-			print 'REG', 'QUERY', '"'+ text +'"'
-			print 'about to do this QUERY: ', reg_query
+			reg_query = 'REG QUERY' +' '+'"'+ text +'"'
+			if os.system(reg_query) == 0:
+				print '"'+ text +'"', ' exists !!!', 'Making backup on', '\n', os.path.abspath(backup_dir), '\n'
+				backup_filename = (text.split('\\')[-1]) + '.reg'
+				reg_export = 'REG EXPORT ' +'"'+ text +'"'+ ' ' +'"'+ backup_dir +'\\'+ backup_filename +'"'
+				if os.system(reg_export) == 0:
+					print backup_filename, ' - stored.', '\n'
+					
+				else:
+						print 'There is an error durring save', backup_filename, 'to', '\n', os.path.abspath(backup_dir), '\n'
 						
-			# (status, output) = commands.getstatusoutput(reg_query)
-			# if status:
-				# sys.stderr.write('there was a QUERY error: ' + output + '\n')
-				# sys.exit(1)
-			# else:
-			backup_filename = (text.split('\\')[-1]) + '.reg'
+			else:
+				print '"'+ text +'"', "-doesn't exist inside win register", '\n'
+						
 				
-			print 'Making backup of', text, '\n', 'in', os.path.abspath(backup_dir), '\n'
-			
-			#reg_export = 'REG EXPORT ' + text, ' ', '"' + backup_dir + '\\' + backup_filename
-			print 'REG EXPORT ' + '"'+ text +'"', ' ', '"'+ backup_dir +'\\'+ backup_filename +'"'
-			
-			reg_export = subprocess.call(['REG', 'EXPORT', '"'+ text +'"' +' ' +'"'+ backup_dir +'\\'+ backup_filename +'"'])
-			print 'about to do this EXPORT: ', reg_export
-				
-			# (status, output) = commands.getstatusoutput(reg_export)
-			# if status:
-				# sys.stderr.write('there was an EXPORT error: ' + output + '\n')
-				# #sys.exit(1)
-			# else:
-			print backup_filename, ' - stored.'
-		
-		
-		
   
 def main():
 	args = sys.argv[1:]
@@ -109,7 +86,7 @@ def main():
 
 	else:
 		#print List(sys.argv[1])
-		backup_and_remove(List(sys.argv[1]))
+		backup(List(sys.argv[1]))
 		
 if __name__ == '__main__':
 	main()
