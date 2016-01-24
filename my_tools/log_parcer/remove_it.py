b503d8570b609498dc698a78544194d5df7323d4
serg060606 <serg060606@gmail.com>
2016-01-25 01:51:13 +0200

    1. argparse() function was added. … 2. items_to_clean_up() function has been changed to avoid junk output in case of lack suited input file data
    3. Couple of code optimisations.

--- my_tools/log_parcer/remove_it.py
+++ my_tools/log_parcer/remove_it.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python2.7 -tt
 
-import re, os, sys, commands, shutil
+import re, os, sys, commands, shutil, argparse
 
 
-def items_to_cleanup(mixed_txt_file):
+def items_to_clean_up(mixed_txt_file):
 	file_open = open(mixed_txt_file, 'rU')
 	content = file_open.read()
   
@@ -14,27 +14,29 @@ def items_to_cleanup(mixed_txt_file):
 		sys.exit(1)
 
 	target_list = re.findall('[A-Z]:.+|HKEY.+', content)
+	list_of_items_to_clean_up = []
 	
-	list_of_items_to_cleanup = []
-	for idx in range(len(target_list)):
-		if 'Mozilla\Firefox\Profiles' in target_list[idx]:
-			match = re.search(r'[A-Z]:\\\w+\\\w+\\\w+\\\w+\\Mozilla\\Firefox\\Profiles\\\w+\.\w+', content)
-			if match.group() not in list_of_items_to_cleanup:
-				list_of_items_to_cleanup.append(match.group())
+	for content in target_list:
+
+		if content.startswith('HKEY') and os.system('REG QUERY "{}" >nul'.format(content)) == 0 or os.path.exists(content):
+			list_of_items_to_clean_up.append(content)
+
 		else:
-			list_of_items_to_cleanup.append(target_list[idx])
-		
-	return list_of_items_to_cleanup
+			print content, '- not found'
 	
-	
-def backup_before_cleanup(list_of_items_to_cleanup):
+	if len(list_of_items_to_clean_up) == 0:
+		sys.exit(1)
+	return list_of_items_to_clean_up
+
+
+def backup_before_clean_up(list_of_items_to_clean_up):
 	backup_dir = 'remove_it_backup'
 	if not os.path.exists(backup_dir):
 		os.mkdir(backup_dir)
 	backup_dir = (os.path.abspath(backup_dir) + '\\')
 				
-	for content in list_of_items_to_cleanup:
-		if re.search(r'\w:\\', content) and not os.path.isdir(content):
+	for content in list_of_items_to_clean_up:
+		if not os.path.isdir(content) and os.path.exists(content):
 			backup_destination = backup_dir + '\\'.join((content.split('\\'))[1:-1])
 			print 'Making backup of', content, '\n', 'in', backup_dir, '\n'
 			if not os.path.exists(backup_destination):
@@ -46,44 +48,35 @@ def backup_before_cleanup(list_of_items_to_cleanup):
 			if not os.path.exists(backup_destination):
 				os.makedirs(backup_destination)
 
-		elif content.startswith('HKEY'):
-			reg_query = 'REG QUERY "{}"'.format(content)
-			if os.system(reg_query) == 0:
-				print '"{}"'.format(content), 'found', 'Making backup on', '\n', os.path.abspath(backup_dir), '\n'
-				backup_filename = (content.split('\\')[-1]) + '.reg'
-				reg_export = 'REG EXPORT "{}"'.format(content) + ' ' + '"{}" /y'.format(backup_dir + backup_filename)
-								
-				if os.system(reg_export) == 0:
-					print 'file', backup_filename, '- done', '\n'
-					
-				else:
-					print reg_export, '\n', 'There is an error durring save', backup_filename, 'to', '\n', os.path.abspath(backup_dir), '\n'
 		else:
-			print  '"{}"'.format(content), "- doesn't exist", '\n'
-			
-def cleanup_by_sourcelist(list_of_items_to_cleanup):
+			backup_filename = (content.split('\\')[-1]) + '.reg'
+			reg_export = 'REG EXPORT "{}"'.format(content) + ' ' + '"{}" /y'.format(backup_dir + backup_filename)
+			print '"{}"'.format(content), 'found', 'Making backup on', '\n', os.path.abspath(backup_dir), '\n'
+			if os.system(reg_export) == 0:
+				print 'file', backup_filename, '- done', '\n'
+					
+			else:
+				print reg_export, '\n', 'There is an error durring save', backup_filename, 'to', '\n', os.path.abspath(backup_dir), '\n'
+	
+
+def clean_up_by_sourcelist(list_of_items_to_clean_up):
 	list_of_empty_dirs = []
 	couter_of_cleaned_items = 0
-	couter_of_items_to_cleanup = len(list_of_items_to_cleanup)
-	for content in list_of_items_to_cleanup:
-		if not content.startswith('HKEY'):
-			# print 'working under: ', content, '\n'
-			cleanup_cmd = 'del /F /Q "{}"'.format(content)
-			if content not in list_of_empty_dirs:
-				list_of_empty_dirs.append(content)
-			
+	couter_of_items_to_clean_up = len(list_of_items_to_clean_up)
+	for content in list_of_items_to_clean_up:
+		if os.path.exists(content):
+			clean_up_cmd = 'del /F /Q "{}"'.format(content)
+			list_of_empty_dirs.append(content)
+
 		else:
-			cleanup_cmd = 'REG DELETE "{}" /f'.format(content)
-			
-
-		if os.system(cleanup_cmd) == 0:
-			print content, '- removed'
+			clean_up_cmd = 'REG DELETE "{}" /f'.format(content)
+		
+		if os.system(clean_up_cmd) == 0:
 			couter_of_cleaned_items += 1
-	
-	empty_dirs_removal(list_of_empty_dirs)
-	summary = 'items found: {}'.format(couter_of_items_to_cleanup) + '\n' + 'items cleaned: {}'.format(couter_of_cleaned_items) + '\n'
-	return summary
 
+	empty_dirs_removal(list_of_empty_dirs)
+	summary = 'items found: {}, cleaned: {}'.format(couter_of_items_to_clean_up, couter_of_cleaned_items)
+	return summary
 
 def empty_dirs_removal(list_of_empty_dirs):
 	
@@ -91,29 +84,28 @@ def empty_dirs_removal(list_of_empty_dirs):
 		path_to_empty_dir = '\\'.join((content.split('\\'))[:-1])
 		
 		if not os.path.isdir(content) and os.path.exists(path_to_empty_dir) and len(os.listdir(path_to_empty_dir)) == 0:
-			print 'last file was removed, there is an empty dir :', '\n', path_to_empty_dir, 'removed', '\n'
+			print 'last file was removed, there is an empty dir :', '\n', path_to_empty_dir, '- removed', '\n'
 			shutil.rmtree(path_to_empty_dir)
 	
 		elif os.path.isdir(content) and len(os.listdir(content)) == 0:
-			print '\n', 'there is an empty dir: ', content, 'removed', '\n'
+			print '\n', 'there is an empty dir: ', content, '- removed', '\n'
 			shutil.rmtree(content)
 	else:
 		pass
   
+
 def main():
-	args = sys.argv[1:]
- 
-	if not args:
-		print 'usage: < --nobackup > logfile'
-		sys.exit(1)
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('textfile', help='file, with list of paths to file/dir or REG value')
+	parser.add_argument('-n', '--nobackup', action='count', help='skip the backup of data before clean-up')
+	args = parser.parse_args()
 	
-	elif args[0] == '--nobackup':
-		print cleanup_by_sourcelist(items_to_cleanup(sys.argv[-1]))
+	if not args.nobackup:
+		backup_before_clean_up(items_to_clean_up(args.textfile))
+	print clean_up_by_sourcelist(items_to_clean_up(args.textfile))
 	
-    
-	else:
-		backup_before_cleanup(items_to_cleanup(sys.argv[1]))
-		cleanup_by_sourcelist(items_to_cleanup(sys.argv[1]))
-		
+
+	
 if __name__ == '__main__':
 	main()
